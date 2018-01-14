54a3bee7c7b4ad139a04fcb0dba41e54c4231443
verchol <verchol@gmail.com>
2018-01-14 03:26:56 +0200

    add annotation test

--- test/images.annotations.spec.js
+++ test/images.annotations.spec.js
@@ -31,21 +31,64 @@ describe('image annotation',  async ()=>{
   })
   const Table = require('cli-table');
   var table = new Table({
-      head: ['Image Name', "Tags"], colWidths: [50, 150]
+      head: ['Image Name', "Tags", "Annotations"], colWidths: [50, 80, 80]
     });
   let draw = (image)=>{
 
-   table.push([image.imageDisplayName, JSON.stringify(image.tags)]);
+   table.push([image.imageDisplayName, JSON.stringify(image.tags), image.annotations]);
 
   }
-  it('annotate', (done)=>{
-   let imageStream = kefir.fromPromise(Images.getAll({})).map((images)=>{
-    let result =  _.chain(images).map((image)=>{
-     let i =  _.pick(image, ["imageDisplayName", "tags", "annotations"]);
-     return i;
-   }).groupBy((image)=>image.imageDisplayName).value();
-     _.map(result , (image)=>{
-        draw({imageDisplayName:_.first(image).imageDisplayName, tags :_.map(image, (i)=>_.get(i, "tags[0].tag", "")).join(' ')});
+
+  it('get image annotation', (done)=>{
+    let imageStream = kefir.fromPromise(Images.getAll({})).flatten().take(1000)
+  //  kefir.sequentially(1000, [{internalImageId:"9dd8b5a1f0a931cab42d226ae849ba817d35861c4843cc80d5f52f42f2fb0eda"}])
+    .flatMap((image)=>{
+      if (!image.internalImageId)
+       return kefir.constantError(image);
+      return kefir.fromPromise(Images.getImageAnnotations(image.internalImageId)).map((annotations)=>{
+        let id = image.internalImageId;
+        let ret = {};
+        _.set(ret, id, annotations);
+        return ret;
+      })
+
+    }).log().onEnd(done, done);
+  })
+
+  it.only('getImageByTag', (done)=>{
+   let imageStream = kefir.fromPromise(Images.getAll({}))
+   let annotations = imageStream.flatten().take(10).flatMap((i)=>{
+          if (!i.internalImageId) return kefir.constant(i);
+          return kefir.fromPromise(Images.getImageAnnotations(i.internalImageId)).map((annotations)=>{
+            _.set(i, "annotations"  ,  annotations);
+            return i;
+    })
+  }).log('images->')
+
+ annotations.log('images->').scan((prev, image)=>{
+     prev.push( _.pick(image, ["imageDisplayName", "tags", "annotations"]));
+     return prev;
+ },[]).map((images)=>{
+     let result =
+     _.chain(images)
+      .map((image)=>{
+         let i =  _.pick(image, ["imageDisplayName", "tags", "annotations"]);
+         return i;})
+      .groupBy((image)=>image.imageDisplayName).value();
+      _.map(result , (image)=>{
+        draw({imageDisplayName:_.first(image).imageDisplayName
+          , tags :_.map(image, (i)=>_.get(i, "tags[0].tag", "")).join(' ')
+          ,annotations :_.map(image, (i)=>{
+           let annotations =   _.get(i, "annotations", {});
+            let ret  = [];
+            _.forIn(annotations, (value, key)=>{
+             ret.push(key + "=" +   value );
+          })
+          let r =   ret.join('\n')
+          console.log(r);
+          return r;
+        })
+        });
         return image;
      });
      return  result;
@@ -56,5 +99,5 @@ describe('image annotation',  async ()=>{
 
 
 
-  })
+ })
 })
